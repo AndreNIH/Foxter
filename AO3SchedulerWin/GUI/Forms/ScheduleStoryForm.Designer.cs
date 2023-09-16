fc344e77968aec7699ce493c7ba502971248ef6d
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2023-09-15 18:14:22 -0700

    Anchor loading image in schedulestory form to the top, right and left positions of the screen

--- AO3SchedulerWin/GUI/Forms/ScheduleStoryForm.Designer.cs
+++ AO3SchedulerWin/GUI/Forms/ScheduleStoryForm.Designer.cs
@@ -57,10 +57,9 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(47, 30);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(33, 18);
             label1.Name = "label1";
-            label1.Size = new Size(104, 25);
+            label1.Size = new Size(68, 15);
             label1.TabIndex = 0;
             label1.Text = "Select Story";
             // 
@@ -68,10 +67,9 @@
             // 
             comboBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(153, 25);
-            comboBox1.Margin = new Padding(4, 5, 4, 5);
+            comboBox1.Location = new Point(107, 15);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(450, 33);
+            comboBox1.Size = new Size(316, 23);
             comboBox1.TabIndex = 1;
             // 
             // dateTimePicker1
@@ -79,69 +77,62 @@
             dateTimePicker1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             dateTimePicker1.CustomFormat = "dd MMM yy hh:mm tt";
             dateTimePicker1.Format = DateTimePickerFormat.Custom;
-            dateTimePicker1.Location = new Point(153, 73);
-            dateTimePicker1.Margin = new Padding(4, 5, 4, 5);
+            dateTimePicker1.Location = new Point(107, 44);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(450, 31);
+            dateTimePicker1.Size = new Size(316, 23);
             dateTimePicker1.TabIndex = 2;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(11, 83);
-            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Location = new Point(8, 50);
             label2.Name = "label2";
-            label2.Size = new Size(140, 25);
+            label2.Size = new Size(93, 15);
             label2.TabIndex = 3;
             label2.Text = "Publishing Date:";
             // 
             // richTextBox1
             // 
             richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            richTextBox1.Location = new Point(11, 198);
-            richTextBox1.Margin = new Padding(4, 5, 4, 5);
+            richTextBox1.Location = new Point(8, 119);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(591, 216);
+            richTextBox1.Size = new Size(415, 131);
             richTextBox1.TabIndex = 4;
             richTextBox1.Text = "";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(11, 168);
-            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Location = new Point(8, 101);
             label3.Name = "label3";
-            label3.Size = new Size(155, 25);
+            label3.Size = new Size(103, 15);
             label3.TabIndex = 5;
             label3.Text = "Chapter Summary";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(11, 438);
-            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Location = new Point(8, 263);
             label4.Name = "label4";
-            label4.Size = new Size(126, 25);
+            label4.Size = new Size(83, 15);
             label4.TabIndex = 7;
             label4.Text = "Chapter Notes";
             // 
             // richTextBox2
             // 
             richTextBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            richTextBox2.Location = new Point(11, 468);
-            richTextBox2.Margin = new Padding(4, 5, 4, 5);
+            richTextBox2.Location = new Point(8, 281);
             richTextBox2.Name = "richTextBox2";
-            richTextBox2.Size = new Size(591, 216);
+            richTextBox2.Size = new Size(415, 131);
             richTextBox2.TabIndex = 6;
             richTextBox2.Text = "";
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(11, 698);
-            checkBox1.Margin = new Padding(4, 5, 4, 5);
+            checkBox1.Location = new Point(8, 419);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(220, 29);
+            checkBox1.Size = new Size(147, 19);
             checkBox1.TabIndex = 8;
             checkBox1.Text = "Notes at the beginning";
             checkBox1.UseVisualStyleBackColor = true;
@@ -149,10 +140,9 @@
             // checkBox2
             // 
             checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(11, 740);
-            checkBox2.Margin = new Padding(4, 5, 4, 5);
+            checkBox2.Location = new Point(8, 444);
             checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(170, 29);
+            checkBox2.Size = new Size(113, 19);
             checkBox2.TabIndex = 9;
             checkBox2.Text = "Notes at the end";
             checkBox2.UseVisualStyleBackColor = true;
@@ -164,10 +154,9 @@
             mainContent.Controls.Add(tabPage3);
             mainContent.Dock = DockStyle.Fill;
             mainContent.Location = new Point(0, 0);
-            mainContent.Margin = new Padding(4, 5, 4, 5);
             mainContent.Name = "mainContent";
             mainContent.SelectedIndex = 0;
-            mainContent.Size = new Size(627, 932);
+            mainContent.Size = new Size(439, 559);
             mainContent.TabIndex = 10;
             // 
             // tabPage1
@@ -183,11 +172,10 @@
             tabPage1.Controls.Add(label2);
             tabPage1.Controls.Add(label3);
             tabPage1.Controls.Add(richTextBox1);
-            tabPage1.Location = new Point(4, 34);
-            tabPage1.Margin = new Padding(4, 5, 4, 5);
+            tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(4, 5, 4, 5);
-            tabPage1.Size = new Size(619, 894);
+            tabPage1.Padding = new Padding(3, 3, 3, 3);
+            tabPage1.Size = new Size(431, 531);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "tabPage1";
             tabPage1.UseVisualStyleBackColor = true;
@@ -195,10 +183,9 @@
             // button1
             // 
             button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            button1.Location = new Point(44, 797);
-            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Location = new Point(31, 478);
             button1.Name = "button1";
-            button1.Size = new Size(539, 75);
+            button1.Size = new Size(377, 45);
             button1.TabIndex = 10;
             button1.Text = "Next";
             button1.UseVisualStyleBackColor = true;
@@ -208,11 +195,10 @@
             tabPage2.BackColor = Color.White;
             tabPage2.Controls.Add(label5);
             tabPage2.Controls.Add(pictureBox1);
-            tabPage2.Location = new Point(4, 34);
-            tabPage2.Margin = new Padding(4, 5, 4, 5);
+            tabPage2.Location = new Point(4, 24);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(4, 5, 4, 5);
-            tabPage2.Size = new Size(619, 894);
+            tabPage2.Padding = new Padding(3, 3, 3, 3);
+            tabPage2.Size = new Size(431, 531);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "tabPage2";
             // 
@@ -221,30 +207,30 @@
             label5.AutoSize = true;
             label5.Font = new Font("Calibri", 18F, FontStyle.Regular, GraphicsUnit.Point);
             label5.ForeColor = SystemColors.ControlDarkDark;
-            label5.Location = new Point(174, 487);
-            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Location = new Point(122, 292);
             label5.Name = "label5";
-            label5.Size = new Size(290, 44);
+            label5.Size = new Size(194, 29);
             label5.TabIndex = 1;
             label5.Text = "Retrieving works...";
             // 
             // pictureBox1
             // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(167, 128);
-            pictureBox1.Margin = new Padding(4, 5, 4, 5);
+            pictureBox1.Location = new Point(117, 77);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(293, 340);
+            pictureBox1.Size = new Size(205, 204);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // tabPage3
             // 
             tabPage3.Controls.Add(richTextBox3);
-            tabPage3.Location = new Point(4, 34);
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Margin = new Padding(2, 2, 2, 2);
             tabPage3.Name = "tabPage3";
-            tabPage3.Padding = new Padding(3);
-            tabPage3.Size = new Size(619, 894);
+            tabPage3.Padding = new Padding(2, 2, 2, 2);
+            tabPage3.Size = new Size(431, 531);
             tabPage3.TabIndex = 2;
             tabPage3.Text = "tabPage3";
             tabPage3.UseVisualStyleBackColor = true;
@@ -252,19 +238,19 @@
             // richTextBox3
             // 
             richTextBox3.Dock = DockStyle.Fill;
-            richTextBox3.Location = new Point(3, 3);
+            richTextBox3.Location = new Point(2, 2);
+            richTextBox3.Margin = new Padding(2, 2, 2, 2);
             richTextBox3.Name = "richTextBox3";
-            richTextBox3.Size = new Size(613, 888);
+            richTextBox3.Size = new Size(427, 527);
             richTextBox3.TabIndex = 0;
             richTextBox3.Text = "";
             // 
             // ScheduleStoryForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(627, 932);
+            ClientSize = new Size(439, 559);
             Controls.Add(mainContent);
-            Margin = new Padding(4, 5, 4, 5);
             Name = "ScheduleStoryForm";
             Text = "ScheduleStoryForm";
             Load += ScheduleStoryForm_Load;
